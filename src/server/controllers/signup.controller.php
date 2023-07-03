6592759769a8bf5ed511e8e2943d9ceecc18777e
Scienitive <scienitive77@gmail.com>
2023-07-03 21:30:02 +0300

    switched to mysql

--- src/server/controllers/signup.controller.php
+++ src/server/controllers/signup.controller.php
@@ -1,3 +1,11 @@
 <?php
-    print_r($_POST);
-?>
+
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) || empty($_POST['uname']) || empty($_POST['password'])) {
+    die('Incorrect Input.');
+}
+
+$hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+
+print_r($_POST);
+var_dump($hashed_password);
