a0f133607ffef3058229bfc636a54fffad1f18eb
Scienitive <scienitive77@gmail.com>
2023-07-25 19:32:04 +0300

    now i need to delete all canvas related stuff :|

--- src/public/js/createPost.js
+++ src/public/js/createPost.js
@@ -1,18 +1,88 @@
-
 export const setCreatePost = async () => {
     const videoElement = document.getElementById('webcam');
     const noWebcamElement = document.getElementById('no-webcam');
-    const imageElement = document.getElementById('the-image');
+    const canvas = document.getElementById('canvas');
     const imageInput = document.getElementById('image-input');
     const captureButton = document.getElementById('capture-button');
     const uploadButton = document.getElementById('upload-button');
     const postButton = document.getElementById('post-button');
     const cancelButton = document.getElementById('cancel-button');
+    const stickerContainer = document.getElementById('sticker-container');
+    const previewContainer = document.getElementById('preview-container');
+    const stickerElements = document.getElementsByClassName('sticker');
     let mediaStream = null;
     let captureMode = true;
 
+    // Canvas
+    let baseImage = null;
+    let stickerArray = [];
+
+    class Sticker {
+        constructor(width, height, scaleX, scaleY, imageUrl) {
+            this.x = 0;
+            this.y = 0;
+            this.width = width;
+            this.height = height;
+            this.scaleX = scaleX;
+            this.scaleY = scaleY;
+            this.image = new Image();
+            this.image.width = 10;
+            this.image.height = 10;
+            this.image.src = imageUrl;
+        }
+    }
+
+    const startCanvas = (width, height, image) => {
+        const ctx = canvas.getContext('2d');
+        canvas.classList.remove('d-none');
+
+        canvas.width = width;
+        canvas.height = height;
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+        baseImage = new Image();
+        baseImage.src = canvas.toDataURL();
+    }
+
+    const drawCanvas = async () => {
+        const ctx = canvas.getContext('2d');
+
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw the base image
+        ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+
+        // Draw stickers
+        for (const sticker of stickerArray) {
+            setTimeout(() => {
+            ctx.drawImage(sticker.image, canvas.width / 2 + sticker.x, canvas.height / 2 + sticker.y);
+            }, 1000);
+        }
+    }
+
+    const endCanvas = () => {
+        const ctx = canvas.getContext('2d');
+        canvas.classList.add('d-none');
+
+        baseImage = null;
+        stickerArray = [];
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    canvas.addEventListener('mousemove', () => {
+        for (let i = stickerArray.length - 1; i >= 0; i--) {
+
+        }
+    });
+
+    canvas.addEventListener('mousedown', () => {
+        for (let i = stickerArray.length - 1; i >= 0; i--) {
+
+        }
+    });
+
     const startWebcam = async () => { // USE WITH AWAIT
-        imageElement.classList.add('d-none');
+        endCanvas();
         try {
             const constraints = { video: true };
             mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -34,7 +104,6 @@ export const setCreatePost = async () => {
         }
         videoElement.classList.add('d-none');
         noWebcamElement.classList.add('d-none');
-        imageElement.classList.remove('d-none');
     }
 
     const changeMode = async () => { // USE WITH AWAIT
@@ -45,6 +114,8 @@ export const setCreatePost = async () => {
             uploadButton.classList.remove('d-none');
             postButton.classList.add('d-none');
             cancelButton.classList.add('d-none');
+            previewContainer.classList.remove('d-none');
+            stickerContainer.classList.add('d-none');
         }
         else {
             stopWebcam();
@@ -52,16 +123,30 @@ export const setCreatePost = async () => {
             uploadButton.classList.add('d-none');
             postButton.classList.remove('d-none');
             cancelButton.classList.remove('d-none');
+            previewContainer.classList.add('d-none');
+            stickerContainer.classList.remove('d-none');
         }
     }
 
+    const createPreview = (imageUrl) => {
+        const rowElement = document.createElement('div');
+        rowElement.classList.add('row');
+        const colElement = document.createElement('div');
+        colElement.classList.add('col');
+        colElement.classList.add('mx-2');
+        colElement.classList.add('my-3');
+        const imageElement = document.createElement('img');
+        imageElement.src = imageUrl;
+        imageElement.classList.add('preview-image');
+
+        previewContainer.appendChild(rowElement);
+        rowElement.appendChild(colElement);
+        colElement.appendChild(imageElement);
+    }
+
     captureButton.addEventListener('click', async () => {
-        const canvas = document.createElement('canvas');
-        canvas.width = videoElement.videoWidth;
-        canvas.height = videoElement.videoHeight;
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-        imageElement.src = canvas.toDataURL();
+        startCanvas(videoElement.videoWidth, videoElement.videoHeight, videoElement);
+        createPreview(canvas.toDataURL());
         await changeMode();
     });
 
@@ -73,7 +158,13 @@ export const setCreatePost = async () => {
         const file = imageInput.files[0];
         const reader = new FileReader();
         reader.onload = (event) => {
-            imageElement.src = event.target.result;
+            const uploadedImage = new Image();
+            uploadedImage.onload = () => {
+                startCanvas(uploadedImage.width, uploadedImage.height, uploadedImage);
+            }
+            uploadedImage.src = event.target.result;
+            createPreview(event.target.result);
+            uploadedImage.remove();
         }
         reader.readAsDataURL(file);
         imageInput.value = '';
@@ -83,4 +174,20 @@ export const setCreatePost = async () => {
     cancelButton.addEventListener('click', async () => {
         await changeMode();
     });
+
+    for (const stickerElement of stickerElements) {
+        stickerElement.addEventListener('click', async (event) => {
+            const width = event.target.naturalWidth;
+            const height = event.target.naturalHeight;
+            let scaleX = 1;
+            let scaleY = 1;
+            const stickerInitialSize = 10; // The bigger the value it gets smaller
+            while (width * scaleX > canvas.width / stickerInitialSize || height * scaleY > canvas.width / stickerInitialSize) {
+                scaleX -= 0.1;
+                scaleY -= 0.1;
+            }
+            stickerArray.push(new Sticker(width, height, scaleX, scaleY, event.target.src));
+            await drawCanvas();
+        });
+    }
 }
