cf074804a88e6338b4fe34538a20647988abb4b5
Scienitive <scienitive77@gmail.com>
2023-07-28 17:57:54 +0300

    few changes on sql

--- tools/mysql/init.sql
+++ tools/mysql/init.sql
@@ -47,10 +47,10 @@ INSERT INTO posts (created_at, user_id, image)
 VALUES ('2023-07-22 13:11:12', 1, 'https://store.donanimhaber.com/73/d4/06/73d406f9b732575d70b83cab86577403.png');
 
 INSERT INTO posts (created_at, user_id, image)
-VALUES ('2023-07-22 13:11:12', 1, '/public/uploads/ee.png');
+VALUES ('2023-07-22 13:11:12', 1, 'https://cdn.discordapp.com/attachments/588224082789662720/1077706545237872701/02d50539-7694-4790-b8ca-fd57be63edc0.jpg');
 
 INSERT INTO posts (created_at, user_id, image)
-VALUES ('2023-07-22 13:11:12', 1, '/public/uploads/352.png');
+VALUES ('2023-07-22 13:11:12', 1, 'https://cdn.discordapp.com/attachments/588224082789662720/1071590375661240380/image.png');
 
 INSERT INTO posts (created_at, user_id, image)
 VALUES ('2023-07-22 13:11:12', 1, 'https://cdn.discordapp.com/attachments/588224082789662720/1091453249183301802/image.png');
