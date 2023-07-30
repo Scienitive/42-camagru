877c61a897cd6c173b9321c9fe276735a6a311ec
Scienitive <scienitive77@gmail.com>
2023-07-31 00:19:09 +0300

    database credentials .env'ed

--- src/server/models/database.php
+++ src/server/models/database.php
@@ -1,15 +1,16 @@
 <?php
 
-$host = "db";
-$dbname = "camagru";
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];;
 $username = "root";
-$password = "123";
+$password = $_ENV['DB_PASS'];;
 
 $dsn = "mysql:host=" . $host . ";dbname=" . $dbname;
 
 try {
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     die("PDO Connection Failed: " . $e->getMessage());
